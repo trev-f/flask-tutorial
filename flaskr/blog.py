6735b7e4baa44fce53797660a79f8f7d3204e503
Trevor F. Freeman <trevor.freeman@utexas.edu>
2023-01-11 14:47:23 -0500

    Add delete blog post

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -76,6 +76,17 @@ def update(id):
     return render_template('blog/update.html', post=post)
 
 
+@bp.route('/<int:id>/delete', methods=('POST',))
+@login_required
+def delete(id):
+    get_post(id)
+    db = get_db()
+    db.execute('DELETE FROM post WHERE id = ?', (id,))
+    db.commit()
+
+    return redirect(url_for('blog.index'))
+    
+
 def get_post(id, check_author=True):
     post = get_db().execute(
         'SELECT p.id, title, body, created, author_id, username'
